140a635b97a1a3d002dc018d65c5a6f3859ec3fd
Stefano Viel <34980834+stefanoviel@users.noreply.github.com>
2021-12-02 10:46:46 +0100

    primo

--- index.js
+++ index.js
@@ -5,12 +5,18 @@ const app = express();
 const { dirname } = require('path');
 const appDir = dirname(require.main.filename);
 
-
 app.get('/',function(req,res) {
     res.sendFile(appDir + '/index.html');
   });
 
+app.get('/idee_viaggio.html',function(req,res) {
+    res.sendFile(appDir + '/idee_viaggio.html');
+  });
+
 app.listen(process.env.port || 3000);
 
-const pene = 5;
+
+
+
+
 
